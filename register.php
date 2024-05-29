f1a833ac46aee1ffeae484e895235d9eef637f37
bimarizqullah <cahyabima548@gmail.com>
2024-05-29 11:48:50 +0700

    banuri ali beban

--- register.php
+++ register.php
@@ -1,45 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+  <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>HANZ STORE | Register</title>
+    <title>HANZ STORE | Login</title>
     <link rel="stylesheet" href="styles2.css" />
-    <link rel="stylesheet" type="text/css" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
-</head>
-<body>
-    <div class="register">
-        <div class="register-container">
-            <div class="register-left">
-                <img src="gambar/logo.jpg" alt="Logo"/>
-            </div>
-            <div class="register-right">
-                <form>
-                    <label for="username">Username</label>
-                    <input type="text" id="username" name="username" required />
+    <link rel="stylesheet" type="text" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+  </head>
+  <body>
+    <div class="login">
+    <div class="login-container">
+      <div class="login-left">
+        <img src="gambar/logo.jpg" alt="Logo"/>
+      </div>
+      <div class="login-right">
+        <form>
+          <label for="email">Masukkan Email</label>
+          <input type="email" id="email" name="email"/>
 
-                    <label for="email">Email</label>
-                    <input type="email" id="email" name="email" required />
+          <label for="email">Konfirmasi Email</label>
+          <input type="email" id="email" name="email" required />
 
-                    <label for="password">Kata sandi</label>
-                    <input type="password" id="password" name="password" required />
+            
+          <label for="password">Masukkan Kata Sandi</label>
+          <input type="password" id="password" name="password"/>
 
-                    <label for="confirm-password">Konfirmasi Kata sandi</label>
-                    <input type="password" id="confirm-password" name="confirm-password" required />
+          <label for="password">Konfirmasi Kata Sandi</label>
+          <input type="password" id="password" name="password" required />
 
-                    <button type="button" class="register-button" onclick="register()">
-                        Daftar
-                    </button>
+          <button
+            type="button"
+            class="register-button"
+            onclick="halamanLogin()"
+          >
+            Daftar
+          </button>
 
-                    <div class="separator">OR</div>
+          <div class="separator">OR</div>
 
-                    <button type="button" class="login-button" onclick="halamanLogin()">
-                        Masuk
-                    </button>
-                </form>
-            </div>
-        </div>
+          <button type="button" class="login-button" onclick="halamanLogin()">
+            Masuk
+          </button>
+        </form>
+      </div>
     </div>
+    </div>
+    </body>
     <script src="script.js"></script>
-</body>
+  </body>
 </html>
